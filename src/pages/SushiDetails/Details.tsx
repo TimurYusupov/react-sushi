940b8f7207f99caf3115e806692570a3e83bec93
TimurYusupov <timurzek95@gmail.com>
2024-02-04 16:29:31 +0100

    fixed data fetching of Details component and changed some css

--- src/pages/SushiDetails/Details.tsx
+++ src/pages/SushiDetails/Details.tsx
@@ -9,6 +9,7 @@ import { ToastContainer, toast } from 'react-toastify'
 import styles from './Details.module.scss'
 
 type TDetails = {
+   id: number
    img: string
    title: string
    description: string
@@ -23,44 +24,17 @@ const SushiDetails: React.FC = () => {
 
    const dispatch = useAppDispatch()
 
-   const { sushiData } = useSelector((state: RootState) => state.homeSlice)
+   const [data, setData] = useState<TDetails | null>(null)
    const { cartItems } = useSelector((state: RootState) => state.cartSlice)
 
-   const sushiItem = sushiData.find((item) => item.id === Number(id))
    const cartItem = cartItems.find((item) => item.id === Number(id))
-
-   const [data, setData] = useState<TDetails | null>(null)
-
-   const addItemToCart = () => {
-      if (sushiItem) {
-         const item: TCartItem = {
-            id: sushiItem.id,
-            title: sushiItem.title,
-            img: sushiItem.img,
-            price: sushiItem.price,
-            count: 0
-         }
-
-         dispatch(addItem(item))
-
-         toast.success(
-            <span>
-               <strong>{sushiItem.title.toUpperCase()}</strong> added into cart
-            </span>
-         )
-      } else {
-         console.log('Nothing ...')
-      }
-   }
-
-   console.log(sushiItem)
 
    useEffect(() => {
       const fetchDetails = async () => {
          try {
             const response = await fetch(`https://518e0d814bf9a511.mokky.dev/items/${id}`)
-            const data = await response.json()
-            setData(data)
+            const json = await response.json()
+            setData(json)
          } catch (err) {
             console.log('Error during sushi details fetching')
          }
@@ -69,48 +43,75 @@ const SushiDetails: React.FC = () => {
       fetchDetails()
    }, [id])
 
+   const addItemToCart = () => {
+      if (data) {
+         const item: TCartItem = {
+            id: data.id,
+            title: data.title,
+            img: data.img,
+            price: data.price,
+            count: 0
+         }
+
+         dispatch(addItem(item))
+
+         toast.success(
+            <span>
+               <strong>{data.title.toUpperCase()}</strong> added into cart
+            </span>
+         )
+      } else {
+         toast.error(<span>Could not add into cart</span>)
+      }
+   }
+
    if (!data) {
-      return <h1>Loading...</h1>
+      return (
+         <div className={styles.spinnerContainer}>
+            <img className={styles.spinner} src="/img/spinner.svg" alt="Spinner" />
+         </div>
+      )
    }
 
    return (
-      <section className={styles.details}>
+      <>
          <ToastContainer autoClose={1000} />
 
-         <div className={styles.detailsImage}>
-            <img src={data.img} alt={data.title} />
-         </div>
+         <section className={styles.details}>
+            <div className={styles.detailsImage}>
+               <img src={data.img} alt={data.title} />
+            </div>
 
-         <div className={styles.detailsInfo}>
-            <h2>{data.title}</h2>
-            {data.portionSize > 0 && (
-               <p className={styles.portion}>{data.portionSize} pcs</p>
-            )}
-            {data.vegan && <p className={styles.vegan}>vegan</p>}
-            {data.spicy && <p className={styles.spicy}>spicy</p>}
-            <p className={styles.description}>{data.description}</p>
-            <h3>EUR {data.price.toFixed(2)}</h3>
-         </div>
-
-         <button className={`button ${styles.btn}`} onClick={addItemToCart}>
-            <svg
-               width="12"
-               height="12"
-               viewBox="0 0 12 12"
-               fill="none"
-               xmlns="http://www.w3.org/2000/svg"
-            >
-               <path
-                  d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
-                  fill="white"
-               />
-            </svg>
-            Order
-            {cartItem && cartItem.count > 0 && (
-               <span className={styles.sushiCount}>{cartItem.count}</span>
-            )}
-         </button>
-      </section>
+            <div className={styles.detailsInfo}>
+               <h2>{data.title}</h2>
+               {data.portionSize > 0 && (
+                  <p className={styles.portion}>{data.portionSize} pcs</p>
+               )}
+               {data.vegan && <p className={styles.vegan}>vegan</p>}
+               {data.spicy && <p className={styles.spicy}>spicy</p>}
+               <p className={styles.description}>{data.description}</p>
+               <h3>EUR {data.price.toFixed(2)}</h3>
+               <button className={`button ${styles.btn}`} onClick={addItemToCart}>
+                  <svg
+                     width="12"
+                     height="12"
+                     viewBox="0 0 12 12"
+                     fill="none"
+                     xmlns="http://www.w3.org/2000/svg"
+                  >
+                     <path
+                        d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+                        fill="white"
+                     />
+                  </svg>
+                  Order
+                  {cartItem && cartItem.count > 0 && (
+                     <span className={styles.sushiCount}>{cartItem.count}</span>
+                  )}
+               </button>
+            </div>
+         </section>
+      </>
    )
 }
 
